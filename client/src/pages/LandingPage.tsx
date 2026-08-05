182d9b83317f0a98a23d723a16aff117d578c8da
angeliquegbrl <angeliquegabriella991@gmail.com>
2026-08-06 01:38:35 +0700

    added landing page design only

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,4 +1,6 @@
 import bg from "../assets/bg-landing.png";
+import illust from "../assets/illust.png";
+import { ChevronsUp } from "lucide-react";
 
 export default function LandingPage() {
   return (
@@ -10,7 +12,25 @@ export default function LandingPage() {
         backgroundRepeat: "no-repeat",
       }}
     >
-      landing page
+      <div className="flex h-full flex-col items-center justify-center ">
+        <p
+          style={{ color: "var(--text-color)" }}
+          className="text-7xl font-bold"
+        >
+          BooDi
+        </p>
+        <div className="bg-[#2d68ff]  px-12 text-white rounded-4xl mt-2">
+          <p className="text-sm tracking-[0.4rem]">Book Diary</p>
+        </div>
+
+        <img
+          src={illust}
+          alt="a guy reading book"
+          className="w-80 mt-6 mb-30"
+        />
+        <p>Swipe up to start</p>
+        <ChevronsUp className=" w-10 h-10 " />
+      </div>
     </div>
   );
 }
