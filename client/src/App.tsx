8257a41855594f70af6b4400c4359d2dde451576
Nadhira Saffanah Zahra <nadhirasaffanah@gmai.com>
2026-08-13 19:17:11 +0700

    fix auth path to book detail and fix path to show book cover

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import toast, { Toaster } from "react-hot-toast";
 export default function App() {
   return (
     <div className="h-screen scrollbar-hide flex justify-center bg-gray-300">
-      <div className="relative scrollbar-hide w-97.5 h-screenbg-white">
+      <div className="relative scrollbar-hide w-97.5 h-screen bg-white">
         <Toaster />
         <Outlet />
       </div>
--- client/src/types/book.ts
+++ client/src/types/book.ts
@@ -1,5 +1,12 @@
 export type Book = {
   id: number;
   title: string;
+  price: number;
+  year: number;
+  page: number;
+  language: string;
+  description: string;
   cover: string;
+  genres: string[];
+  authors: { id: number; name: string }[];
 };
