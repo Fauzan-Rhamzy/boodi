9061a7f576b7a89d01fc329956426652c3cb7864
Nadhira Saffanah Zahra <nadhirasaffanah@gmai.com>
2026-08-18 10:52:17 +0700

    FEAT(Favourite-button in book detail page): made the favourite button works, and generate favourite dan currently reading collection for new user

--- client/src/api/collection.ts
+++ client/src/api/collection.ts
@@ -25,3 +25,16 @@ export async function getFavouriteBooks(): Promise<FavouriteBooks[]> {
   const response = await api.get("/favourite-books");
   return response.data.data;
 }
+
+export async function AddToFavourite(bookId: number): Promise<void> {
+  await api.post(`/favourite-books/${bookId}`);
+}
+
+export async function DeleteFromFavourite(bookId: number): Promise<void> {
+  await api.delete(`/favourite-books/${bookId}`);
+}
+
+export async function checkIsFavourited(bookId: number): Promise<boolean> {
+  const response = await api.get(`/favourite-books/check/${bookId}`);
+  return response.data.data.is_favourited;  
+}
