053062e84476f8e738caab63b391e165280cc2b3
Nadhira Saffanah Zahra <nadhirasaffanah@gmai.com>
2026-08-18 12:28:10 +0700

    FIX(profile picture): sesuain sama dummy

--- client/src/components/UserProfile.tsx
+++ client/src/components/UserProfile.tsx
@@ -25,7 +25,7 @@ export default function UserProfile({ pfp }: { pfp: string }) {
         className="flex items-center gap-1 cursor-pointer"
       >
         <img
-          src={`http://localhost:8080/images/${user?.profile_picture}`}
+          src={`http://localhost:8080/images/profile/dummy.png`}
           className="w-10 h-10 rounded-full object-cover"
         />
         <ChevronDown
