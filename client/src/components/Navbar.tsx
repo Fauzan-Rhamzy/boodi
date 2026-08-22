92fbf852fbd1c0597006322a50416a7cce58ccd1
angeliquegbrl <angeliquegabriella991@gmail.com>
2026-08-23 00:53:38 +0700

    FIX(navbar) : added search in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router";
-import { Home, LayoutGrid, User } from "lucide-react";
+import { Home, LayoutGrid, Search, User } from "lucide-react";
 
 export default function Navbar() {
   const location = useLocation();
@@ -7,14 +7,14 @@ export default function Navbar() {
 
   return (
     <div className="fixed bottom-0 left-1/2 z-50 w-full max-w-sm -translate-x-1/2">
-      <nav className="bg-white/95 backdrop-blur-md border border-gray-100 shadow-2xl rounded-t-3xl py-2 px-8 w-full">
+      <nav className="bg-[#533113] backdrop-blur-md border shadow-2xl rounded-t-3xl py-2 px-8 w-full">
         <div className="flex justify-between items-center max-w-sm mx-auto">
           <Link
             to={"/home"}
-            className={`flex flex-col items-center gap-1 p-2 transition-colors duration-200 ${
+            className={` flex flex-col items-center gap-1 p-2 transition-colors duration-200 ${
               isActive("/home")
-                ? "text-pink-500 font-semibold"
-                : "hover:text-pink-500"
+                ? "text-[#FFD49A] font-semibold"
+                : "hover:text-[#FFD49A] text-[#fffdfb]"
             }`}
           >
             <Home className="h-6 w-6 stroke-[1.75]" />
@@ -22,11 +22,23 @@ export default function Navbar() {
           </Link>
 
           <Link
+            to={"/search"}
+            className={`flex flex-col items-center gap-1 p-2 transition-colors duration-200 ${
+              isActive("/search")
+                ? "text-[#FFD49A] font-semibold"
+                : "hover:text-[#FFD49A] text-[#fffdfb] "
+            }`}
+          >
+            <Search className="h-6 w-6 stroke-[1.75]" />
+            <span className="text-[11px]">Search</span>
+          </Link>
+
+          <Link
             to={"/library"}
             className={`flex flex-col items-center gap-1 p-2 transition-colors duration-200 ${
               isActive("/library")
-                ? "text-pink-500 font-semibold"
-                : "hover:text-pink-500"
+                ? "text-[#FFD49A] font-semibold"
+                : "hover:text-[#FFD49A] text-[#fffdfb] "
             }`}
           >
             <LayoutGrid className="h-6 w-6 stroke-[1.75]" />
@@ -37,8 +49,8 @@ export default function Navbar() {
             to={"/profile"}
             className={`flex flex-col items-center gap-1 p-2 transition-colors duration-200 ${
               isActive("/profile")
-                ? "text-pink-500 font-semibold"
-                : "hover:text-pink-500"
+                ? "text-[#FFD49A] font-semibold"
+                : "hover:text-[#FFD49A] text-[#fffdfb] "
             }`}
           >
             <User className="h-6 w-6 stroke-[1.75]" />
