c77cf2dbf9204f15eafe691a7e88f08700c3328d
Fauzan Rhamzy <fauzan.rhamzy01@gmail.com>
2026-08-24 19:29:05 +0700

    FIX(Notifikasi): fixed the notificaton that wouldnt go away after logging in and out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
     <div className="h-screen scrollbar-hide flex justify-center bg-gray-300">
       <div className="relative scrollbar-hide w-full max-w-md min-h-screen bg-bw shadow-xl overflow-x-hidden">
-        <Toaster />
         <Outlet />
       </div>
     </div>
