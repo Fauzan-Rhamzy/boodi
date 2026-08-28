94f5e4cc355fc80aa8a4b78ddeee1ea85f7f41c1
angeliquegbrl <angeliquegabriella991@gmail.com>
2026-08-29 00:19:59 +0700

    added bit of ui

--- client/src/pages/Profile-ReviewPage.tsx
+++ client/src/pages/Profile-ReviewPage.tsx
@@ -3,6 +3,8 @@ import { getMe } from "../features/auth/api";
 import ProfileHeader from "../components/ProfileHeader";
 import type { User } from "../types/users";
 import { getUserProfile } from "../api/users";
+import SearchBar from "../components/SearchBar";
+import TrendingReviewCard from "../components/TrendingReviewCard";
 
 export default function ProfileReviewsPage() {
   const [user, setUser] = useState<User | null>(null);
@@ -22,10 +24,18 @@ export default function ProfileReviewsPage() {
   }, []);
 
   return (
-    <div
-      className="w-full min-h-screen relative pb-24 bg-bw">
-      <div>
+    <div className="w-full min-h-screen relative pb-24 bg-bw">
+      <div className="flex flex-col gap-3 justify-center items-center">
         <ProfileHeader user={user} />
+        <SearchBar className="w-8/10"></SearchBar>
+      </div>
+      <div className="mt-6 pb-30">
+        <div className="flex items-center justify-between mb-2">
+          <p className="text-xl font-bold">Trending Review</p>
+        </div>
+        {trendingReviews.map((review) => (
+          <TrendingReviewCard key={review.review_id} review={review} />
+        ))}
       </div>
     </div>
   );
